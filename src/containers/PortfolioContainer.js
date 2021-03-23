7847ce6ef03c7f82d619a168842dda42658be499
Jacob Hampton <jhampton792@gmail.com>
2021-03-23 12:11:58 -0500

    commit tree ded5841b486cafaf44f4f0c55ec3227a106d2fa1

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -7,9 +7,7 @@ class PortfolioContainer extends Component {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+        {this.props.stocks.length > 0 ? this.props.stocks.map(stock => <Stock stock={stock} />) : <p>Add Stocks By Clicking On Their Card</p>}
       </div>
     );
   }
